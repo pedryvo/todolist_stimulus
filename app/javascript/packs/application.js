5747a547020ea4859414500e5948f8556fa6b754
Pedro Ivo <pedryvo@gmail.com>
2021-02-01 02:11:51 -0300

    create base layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "controllers"
 
+import '../stylesheets/application'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
