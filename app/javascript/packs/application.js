c87067dfc80d15556619f46468284f3071f55b4e
Pedro Ivo <pedryvo@gmail.com>
2021-01-31 23:00:44 -0300

    add stimulusjs bootstrap and jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "controllers"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require('bootstrap/dist/js/bootstrap.bundle')
+require('jquery')
+
